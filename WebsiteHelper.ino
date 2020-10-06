245254a435a10dd7599db716ab00756d09878484
maylortaylor <maylortaylor@gmail.com>
2020-10-06 01:45:00 -0400

    movnig things aroudn

--- WebsiteHelper.ino
+++ WebsiteHelper.ino
@@ -1,6 +1,15 @@
 
 void createRoutes(AsyncWebServer *server)
 {
+  // Route to load style.css file
+  server->on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  server->on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/main.js", "text/javascript");
+  });
+
   // Route for root / web page
   server->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     int paramsNr = request->params();
@@ -18,15 +27,6 @@ void createRoutes(AsyncWebServer *server)
       }
     }
     request->send(SPIFFS, "/index.html", String(), false, websiteProcessor);
-  });
-
-  // Route to load style.css file
-  server->on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-
-  server->on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/main.js", "text/javascript");
   });
 
   // server->on("/obama_not_bad.jpg", HTTP_GET, [](AsyncWebServerRequest *request) {

78faac87592dfd6a0851675c2fe97ac395bad0fa
maylortaylor <maylortaylor@gmail.com>
2020-10-08 16:44:48 -0400

    fixed some spacing on magic game

--- MagicGame.ino
+++ MagicGame.ino
@@ -6,8 +6,10 @@ void magic_game()
 
   if (magicWordsGame.answerMode == false)
   {
-
     u8g2.setCursor(SCREEN_WIDTH_HALF - 30, SCREEN_HEIGHT_HALF);
+    u8g2.print("Question: ");
+
+    u8g2.setCursor(SCREEN_WIDTH_HALF - 60, SCREEN_HEIGHT_HALF + 20);
     u8g2.print(MAGIC_QUESTION[magicWordsGame.randomQuestionNumber]);
 
     if (knob1.click == 1)
@@ -18,6 +20,9 @@ void magic_game()
   else
   {
     u8g2.setCursor(SCREEN_WIDTH_HALF - 30, SCREEN_HEIGHT_HALF);
+    u8g2.print("Answer: ");
+
+    u8g2.setCursor(SCREEN_WIDTH_HALF - 60, SCREEN_HEIGHT_HALF + 20);
     u8g2.print(MAGIC_ANSWERS[magicWordsGame.randomAnswerNumber]);
 
     if (knob1.click == 1)

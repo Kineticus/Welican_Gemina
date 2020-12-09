5f7e4971021ccdd466c61b4e37409c86d2704f81
Matt Taylor <maylortaylor@gmail.com>
2020-12-09 02:34:34 -0500

    Delete Secrets.h

--- Secrets.h
+++
@@ -1,5 +0,0 @@
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-#define OPEN_WEATHER_API_KEY "8f40cb693f2032badf06d4e432e1dfa6"
-#define FIREBASE_URL "welican-gemina-18f94-default-rtdb.firebaseio.com"
-#define FIREBASE_DATABASE_SECRET_KEY "75ef6GnJJQHtfPbIZXdtoAFUhLLdtQ2Px6CD49Oz"
